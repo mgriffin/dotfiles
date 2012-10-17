c77b8b54f9c7688f3622c26d2b99baecc07ea6d3
Mike Griffin <griffinm@gmail.com>
2012-10-17 14:27:03 +0100

    Moves my config around into the custom directory
    So it won't be overwritten with .zshrc

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,4 @@
+alias l="ls -l"
+alias ll="ls -al"
+alias  sz="source ~/.zshrc"
+
---
+++ oh-my-zsh/custom/settings.zsh
@@ -0,0 +1,8 @@
+
+export LANG=en_IE.utf-8
+export LC_ALL=en_IE.utf-8
+
+export VISUAL=vim
+export EDITOR=vim
+export PAGER="less -E"
+
