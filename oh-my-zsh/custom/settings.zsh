9cb1bcec9bbdf1a492b514eb8a3d1ad5a4c5e3ab
Mike Griffin <griffinm@gmail.com>
2013-01-02 10:47:40 +0000

    Changes some zsh history settings to store more data

--- oh-my-zsh/custom/settings.zsh
+++ oh-my-zsh/custom/settings.zsh
@@ -16,8 +16,20 @@ fi
 
 export PATH
 
-export HISTSIZE=2000
-export SAVEHIST=2000
+# export HISTSIZE=200000
+# export SAVEHIST=200000
+# setopt EXTENDED_HISTORY
+setopt INC_APPEND_HISTORY   # append history list to the history file (important for multiple parallel zsh sessions!)
+setopt SHARE_HISTORY        # import new commands from the history file also in other zsh-session
+setopt EXTENDED_HISTORY     # save each command's beginning timestamp and the duration to the history file
+setopt HIST_IGNORE_ALL_DUPS # If  a  new  command  line being added to the history
+                            # list duplicates an older one, the older command is removed from the list
+setopt HIST_IGNORE_SPACE    # remove command lines from the history list when
+                            # the first character on the line is a space
+# set a huge history... doesn't seem to affect performance at all
+export HISTFILE=~/.zsh_history
+export HISTSIZE=10000
+export SAVEHIST=$((HISTSIZE/2))
 
 export GEM_HOME="$HOME/.gems"
 export GEM_PATH="$GEM_HOME:/usr/lib/ruby/gems/1.8"
