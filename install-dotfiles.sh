6dbcc45ae4b53219b8f1e918580c8e6e9f95ac57
Mike Griffin <mgriffin@github.com>
2016-01-14 20:25:30 +0000

    Add a file to install my dotfiles
    Shamelessly cogged from @mikemcquaid at
    https://github.com/mikemcquaid/dotfiles

---
+++ install-dotfiles.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Install all dotfiles into the home directory
+
+DOTFILESDIRREL=$(dirname $0)
+cd $DOTFILESDIRREL
+DOTFILESDIR=$(pwd -P)
+SCRIPTNAME=$(basename $0)
+
+[ $(uname -s) = "Darwin" ] && export OSX=1 && export UNIX=1
+[ $(uname -s) = "Linux" ] && export LINUX=1 && export UNIX=1
+uname -s | grep -q "_NT-" && export WINDOWS=1
+
+for DOTFILE in *; do
+  HOMEFILE="$HOME/.$DOTFILE"
+  [ -d $DOTFILE ] && DOTFILE="$DOTFILE/"
+  DIRFILE="$DOTFILESDIR/$DOTFILE"
+
+  echo $DOTFILE | egrep -q '(dotfiles|\.txt|\.md)' && continue
+
+  # Don't install some files unless you're also called Mike.
+  if echo $USER | grep -vq 'mike'
+  then
+    echo $DOTFILE | grep -q 'gitconfig-user' && continue
+    echo $DOTFILE | grep -q 'ssh' && continue
+  fi
+
+  echo $DOTFILE | grep -q '\.sh' \
+    && HOMEFILE="$HOME/.$(echo $DOTFILE | sed -e 's/\.sh//')"
+
+  if [ $UNIX ]
+  then
+    if [ -L "$HOMEFILE" ] && ! [ -d $DOTFILE ]
+    then
+      ln -sfv "$DIRFILE" "$HOMEFILE"
+    else
+      rm -rv "$HOMEFILE"
+      ln -sv "$DIRFILE" "$HOMEFILE"
+    fi
+  else
+    cp -rv "$DIRFILE" "$HOMEFILE"
+  fi
+done
