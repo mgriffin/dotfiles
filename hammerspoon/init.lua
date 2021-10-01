b94d09d25f878b130ae231c5347f126aa26f821e
Mike Griffin <mgriffin@github.com>
2021-10-01 13:32:14 +0100

    Adds a bluetooth toggle bit

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,51 @@
+hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
+hs.loadSpoon('Hyper')                 -- https://github.com/evantravers/Hyper.spoon
+
+local applescript = [[tell application "System Preferences"
+    reveal pane id "com.apple.preferences.Bluetooth"
+    -- activate
+
+    set the current pane to pane id "com.apple.preferences.Bluetooth"
+
+    try
+        tell application "System Events" to tell process "System Preferences"
+            click button "Turn Bluetooth Off" of window "Bluetooth"
+        end tell
+
+        delay 1
+
+    on error
+        tell application "System Events" to tell process "System Preferences"
+            click button "Turn Bluetooth On" of window "Bluetooth"
+        end tell
+
+    end try
+    quit
+
+end tell]]
+
+Config = {}
+Config.applications = require('configApplications')
+
+Hyper = spoon.Hyper
+             :start(Config)
+             :setHyperKey('F19')
+
+-- Jump to zoom
+Hyper:bind({}, 'z', nil, function()
+  hs.application.launchOrFocusByBundleID('com.obsproject.obs-studio')
+  hs.application.launchOrFocusByBundleID('us.zoom.xos')
+end)
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
 end)
 hs.alert.show("🤘 Config loaded 🤘")
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "B", function()
+  hs.osascript.applescript(applescript)
+end)
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -64,20 +108,19 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
 end)
 local screenName = hs.screen.allScreens()[1]:name()
 local laptopWindowLayout = {
-  {"Google Chrome",  nil,          screenName, hs.layout.maximized,    nil, nil},
-  {"Slack",   nil,          screenName, hs.layout.maximized,   nil, nil},
-  {"iTerm2",   nil,          screenName, hs.layout.maximized,   nil, nil},
-  {"Atom",   nil,          screenName, hs.layout.maximized,   nil, nil},
+  {"Google Chrome",  nil, screenName, hs.layout.maximized, nil, nil},
+  {"Slack",          nil, screenName, hs.layout.maximized, nil, nil},
+  {"iTerm2",         nil, screenName, hs.layout.maximized, nil, nil},
+  {"Atom",           nil, screenName, hs.layout.maximized, nil, nil},
 }
 local bigWindowLayout = {
-  {"Google Chrome",  nil,          screenName, {x=0, y=0, w=0.6, h=1},    nil, nil},
-  {"Slack",   nil,          screenName, {x=0.6, y=0, w=0.4, h=1},   nil, nil},
-  {"iTerm2",   nil,          screenName, {x=0, y=0, w=0.6, h=1},   nil, nil},
-  {"Atom",   nil,          screenName, {x=0, y=0, w=0.6, h=1},   nil, nil},
+  {"Google Chrome",  nil, screenName, {x=0,   y=0, w=0.6, h=1}, nil, nil},
+  {"Slack",          nil, screenName, {x=0.6, y=0, w=0.4, h=1}, nil, nil},
+  {"iTerm2",         nil, screenName, {x=0,   y=0, w=0.6, h=1}, nil, nil},
+  {"Atom",           nil, screenName, {x=0,   y=0, w=0.6, h=1}, nil, nil},
 }
-if screenName == "Color LCD" then
+if screenName == "Built-in Retina Display" then
   hs.layout.apply(laptopWindowLayout)
 elseif screenName == "Thunderbolt Display" then
   hs.layout.apply(bigWindowLayout)
 end
-hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
