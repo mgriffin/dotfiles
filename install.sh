1c544de8daa49a02b787a55168c50d963b12c3db
Mike Griffin <mgriffin@github.com>
2021-10-01 15:16:57 +0100

    Remove the default bashrc

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
+rm $HOME/.bashrc
+
 ln -s $(pwd)/bashrc $HOME/.bashrc
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
-source $HOME/.bashrc
 
 apt-get install fzf
 
