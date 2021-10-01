86622c62d40eb5e9fe01a852ff74512284a60b67
Mike Griffin <mgriffin@github.com>
2021-10-01 13:33:11 +0100

    Add a codespace install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+exec > >(tee -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
+ln -s $(pwd)/bashrc $HOME/.bashrc
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/vim $HOME/.vim
+
+vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
