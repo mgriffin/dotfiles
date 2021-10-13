e42dbe2a592a3c253922f8656293ef207ffbeffd
Mike Griffin <mgriffin@github.com>
2021-10-13 12:30:53 +0100

    export a token

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ ln -s $(pwd)/bashrc $HOME/.bashrc
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
 
+echo '[[ -v GH_GH_PAT ]] && export GITHUB_TOKEN=$GH_GH_PAT' >> ~/.bashrc
+
 apt-get install fzf
 
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
