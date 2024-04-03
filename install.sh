55d8e6f994ea9fecbbc38fa03aeae99148dc039f
Mike Griffin <mgriffin@github.com>
2024-04-03 10:54:20 +0100

    Update install script for codespaces

--- install.sh
+++ install.sh
@@ -6,12 +6,15 @@ set -x
 
 rm $HOME/.bashrc
 
+ln -s $(pwd)/git-prompt $HOME/.git-prompt
 ln -s $(pwd)/bashrc $HOME/.bashrc
 ln -s $(pwd)/vimrc $HOME/.vimrc
 ln -s $(pwd)/vim $HOME/.vim
+ln -s $(pwd)/gitconfig-user $HOME/.gitconfig-user
+cat $(pwd)/gitconfig >> $HOME/.gitconfig
 
 echo '[[ -v GH_GH_PAT ]] && export GITHUB_TOKEN=$GH_GH_PAT' >> ~/.bashrc
 
-apt-get install fzf
+sudo apt-get install fzf
 
 vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
